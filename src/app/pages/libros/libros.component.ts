4726d8cff17569cac16aeb1833cc7d1f0ace5420
JuditAF <idryscreep@gmail.com>
2024-05-21 18:51:39 +0200

    subo card sin terminar

--- src/app/pages/libros/libros.component.ts
+++ src/app/pages/libros/libros.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/app/models/book';
 
 @Component({
   selector: 'app-libros',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./libros.component.css']
 })
 export class LibrosComponent {
+
+public myBooks: Book[];
+
+constructor () {
+
+  this.myBooks = [new Book ( 30305, 11, "El guerrero a la sombra del cerezo", "Novela histórica", "David B.Gil", 19.85, "https://m.media-amazon.com/images/I/91AOK0vUY+L._AC_UF894,1000_QL80_.jpg")];
+  
+}
+
+// public modificar (nuevoNombre: HTMLInputElement) {
+  
+//   console.log(this.myUser.name);
+//   this.myUser.name = nuevoNombre.value;
+//   console.log(this.myUser.name);
+
+// }
 
 }
