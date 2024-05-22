386b08f1cfbea8cfc635d57f6638f83bb28d8cfe
JuditAF <idryscreep@gmail.com>
2024-05-22 17:55:09 +0200

    Subo ejercicio con ngFor even

--- src/app/models/book.ts
+++ src/app/models/book.ts
@@ -1,6 +1,7 @@
 export class Book {
 
     public myBook: Book
+    public myBooks: Book[]
 
     constructor(public id_book: number = 0,
                 public id_user: number = 0,
@@ -9,4 +10,12 @@ export class Book {
                 public author: string,
                 public price: number,
                 public photo: string) { }
+
+    public addBook() {
+
+        let myBook = new Book(this.id_book, this.id_user=0, this.title, this.type, this.author, this.price, this.photo);
+        return this.myBooks.push(myBook);
+                
+    }
+
 }
--- src/app/pages/libros/libros.component.ts
+++ src/app/pages/libros/libros.component.ts
@@ -13,21 +13,16 @@ export class LibrosComponent {
   constructor() {
 
     this.myBooks = [
-      new Book(30305, 11, "El guerrero a la sombra del cerezo", "Novela Histórica", "David B.Gil", 19.85, "https://m.media-amazon.com/images/I/91AOK0vUY+L._AC_UF894,1000_QL80_.jpg"),
-      new Book(32305, 4, "Jerusalén", "Novela Fantástica", "Alan Moore", 57, "https://m.media-amazon.com/images/I/7147Uz8Y5qL._AC_UF894,1000_QL80_.jpg"),
-      new Book(31897, 5, "Africanus", "Novela Histórica", "Santiago Posteguillo", 75.33, "https://quelibroleo.com/images/libros/libro_1353882344.jpg"),
+      new Book(30305, 7, "El guerrero a la sombra del cerezo", "Novela Histórica", "David B.Gil", 19.85, "https://m.media-amazon.com/images/I/91AOK0vUY+L._AC_UF894,1000_QL80_.jpg"),
+      new Book(32305, 4, "Trilogía Jerusalén", "Novela Fantástica", "Alan Moore", 57, "https://m.media-amazon.com/images/I/7147Uz8Y5qL._AC_UF894,1000_QL80_.jpg"),
+      new Book(31897, 5, "Trilogía Africanus", "Novela Histórica", "Santiago Posteguillo", 75.33, "https://quelibroleo.com/images/libros/libro_1353882344.jpg"),
       new Book(30621, 2, "Determined", "Divulgación Científica", "Robert sapolsky", 26.60, "https://m.media-amazon.com/images/I/81PX0uhxQ3L._AC_UF894,1000_QL80_.jpg"),
       new Book(27640, 6, "Alex", "Novela Negra", "Pierre Lemaitre", 20.80, "https://www.libreriaalberti.com/media/img/portadas/_visd_0000JPG01NQ5.jpg"),
       new Book(28974, 8, "El arte de la guerra", "Filosofía", "sun Tzu", 17.95, "https://m.media-amazon.com/images/I/61Im39L4o1L._AC_UF894,1000_QL80_.jpg"),
-      new Book(39142, 9, "Dune", "Ciencia Ficción", "Frank Herbert", 59.71, "https://www.libreriafabula.com/imagenes_grandes/9788466/978846636340.JPG")
+      new Book(39142, 9, "Trilogía Dune", "Ciencia Ficción", "Frank Herbert", 59.71, "https://www.libreriafabula.com/imagenes_grandes/9788466/978846636340.JPG"),
+      new Book(36228, 1, "El barco de Teseo", "Narrativa Fantástica", "J.J. Abrams", 49, "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgcaCbDX4HM9ux9Amx44JgtlepwI-bzP56OP5G7XoANFg7S1YpG1f7LrR6lliwuRD226afnRrt41BITcJlNpIckV-QEk8R1sex4DQfsBtNnP09qkhiQ86DbtD1So2nvnNdQPWbjVO89NK9YF0sN-3S_AYcGv08BjMROodf55BxZ-2J-JqUH81IPYh6aJPTx/s764/S.-El-barco-de-Teseo-550x764.jpg"),
+      new Book(33845, 3, "Poesía Completa", "Poesía Extranjera", "Matsuo Basho", 24.7, "https://m.media-amazon.com/images/I/51lzs8eQaRL._AC_UF894,1000_QL80_DpWeblab_.jpg")
     ];
-
-  }
-
-  public addBook(nuevoNombre: string) {
-
-    let myBook1 = new Book(32305, 4, "Jerusalén", "Novela Fantástica", "Alan Moore", 57, "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.amazon.es%2FJerusalem-Liverlight-Alan-Moore%2Fdp%2F1631491342&psig=AOvVaw1t7Vni0csQmOhkgsCEWa1n&ust=1716463422443000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLj87tWSoYYDFQAAAAAdAAAAABBK");
-    return this.myBooks.push(myBook1);
 
   }
 
