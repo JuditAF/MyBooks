f19f449beb5c529f38225aa67ac33c885ebab4c8
JuditAF <idryscreep@gmail.com>
2024-05-27 18:59:22 +0200

    arreglo carpeta app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { LibrosComponent } from './pages/libros/libros.component';
+import { AddBookComponent } from './pages/add-book/add-book.component';
+import { UpdateBookComponent } from './pages/update-book/update-book.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
   {path: "register", component: RegisterComponent},
   {path: "profile", component: ProfileComponent},
-  {path: "libros", component: LibrosComponent}
+  {path: "libros", component: LibrosComponent},
+  {path: "AddBook", component: AddBookComponent},
+  {path: "UpdateBook", component: UpdateBookComponent}
 ];
 
 @NgModule({
